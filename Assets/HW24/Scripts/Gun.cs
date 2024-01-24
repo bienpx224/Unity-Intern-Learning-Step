f2024bfa141f2df14731de7e15454b08b376d2f2
TaAnhSon2302 <son.ta@sotatek.com>
2024-01-24 17:18:04 +0700

    *Change Totalfired number to TMP format

--- Assets/HW24/Scripts/Gun.cs
+++ Assets/HW24/Scripts/Gun.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,12 +9,12 @@ public class Gun : MonoBehaviour
     public GameObject Bullet;
     float fireRate = 0.5f;
     float nextFire = 0;
-    public Text totalFired;
+    public TMP_Text totalFired;
     int count = 0;
     // Start is called before the first frame update
     void Start()
     {
-   
+    
     }
     void Update()
     {
