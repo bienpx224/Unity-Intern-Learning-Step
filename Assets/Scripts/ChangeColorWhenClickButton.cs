47be5a71a1da260a9abede22c1f36a62fef1d147
Sotatek-QuangDao <quang.dao@sotatek.com>
2024-01-24 10:46:56 +0700

    HW 23/1: change color when click button (done)

--- Assets/Scripts/ChangeColorWhenClickButton.cs
+++ Assets/Scripts/ChangeColorWhenClickButton.cs
@@ -6,24 +6,32 @@ using UnityEngine.UI;
 public class ChangeColorWhenClickButton : MonoBehaviour
 {
     public SpriteRenderer squareSprite;
-    public Color blue;
-    public Color yellow;
+    public Button button1;
+    public Button button2;
+    private Color button1Color;
+    private Color button2Color;
     private Color originalSquareColor;
 
     void Start()
     {
         originalSquareColor = squareSprite.color;
+        button1Color = button1.image.color;
+        button2Color = button2.image.color;
     }
 
     public void BlueColor()
     {
-        squareSprite.color = blue;
+        ChangeColor(button1Color);
         StartCoroutine(ResetColorAfterDelay());
     }
     public void YellowColor()
     {
-        squareSprite.color = yellow;
+        ChangeColor(button2Color);
         StartCoroutine(ResetColorAfterDelay());
+    }
+    public void ChangeColor(Color buttonColor)
+    {
+        squareSprite.color = buttonColor;
     }
     IEnumerator ResetColorAfterDelay()
     {
